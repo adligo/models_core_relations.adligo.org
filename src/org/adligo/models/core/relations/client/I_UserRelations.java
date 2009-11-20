ed66672aa3ff93f503921e4f47d01a08b99614e4
scott <scott@adligo.com>
2009-11-20 12:38:35 +0000

    interface extension

--- src/org/adligo/models/core/relations/client/I_UserRelations.java
+++ src/org/adligo/models/core/relations/client/I_UserRelations.java
@@ -4,9 +4,10 @@ import java.util.Set;
 
 import org.adligo.models.core.client.I_Org;
 import org.adligo.models.core.client.I_Person;
+import org.adligo.models.core.client.I_Subject;
 import org.adligo.models.core.client.I_User;
 
-public interface I_UserRelations {
+public interface I_UserRelations extends I_Subject {
 
 	public abstract Set<String> getRoles();
 
