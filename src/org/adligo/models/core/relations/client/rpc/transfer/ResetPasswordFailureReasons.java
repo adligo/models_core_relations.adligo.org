fec33b90d4f766256f7962291cf6111564bffc2e
scott <scott@adligo.com>
2009-12-23 07:18:43 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_4'.
    Sprout from master 2009-12-23 07:18:39 UTC scott <scott@adligo.com> 'updates for a better StorageIdentifier'
    Delete:
        src/org/adligo/models/core/relations/client/rpc/AuthenticationService.java
        src/org/adligo/models/core/relations/client/rpc/AuthenticationServiceAsync.java
        src/org/adligo/models/core/relations/client/rpc/PasswordAssistanceService.java
        src/org/adligo/models/core/relations/client/rpc/PasswordAssistanceServiceAsync.java
        src/org/adligo/models/core/relations/client/rpc/UserEditService.java
        src/org/adligo/models/core/relations/client/rpc/UserEditServiceAsync.java
        src/org/adligo/models/core/relations/client/rpc/UserGroupEditService.java
        src/org/adligo/models/core/relations/client/rpc/UserGroupEditServiceAsync.java
        src/org/adligo/models/core/relations/client/rpc/UserGroupInfoService.java
        src/org/adligo/models/core/relations/client/rpc/UserGroupInfoServiceAsync.java
        src/org/adligo/models/core/relations/client/rpc/UserInfoService.java
        src/org/adligo/models/core/relations/client/rpc/UserInfoServiceAsync.java
        src/org/adligo/models/core/relations/client/rpc/UserNameAssistanceService.java
        src/org/adligo/models/core/relations/client/rpc/UserNameAssistanceServiceAsync.java
        src/org/adligo/models/core/relations/client/rpc/transfer/AuthenticationRequest.java
        src/org/adligo/models/core/relations/client/rpc/transfer/AuthenticationResponse.java
        src/org/adligo/models/core/relations/client/rpc/transfer/EmailAssistanceReponse.java
        src/org/adligo/models/core/relations/client/rpc/transfer/EmailAssistanceRequest.java
        src/org/adligo/models/core/relations/client/rpc/transfer/ResetPasswordFailureReasons.java
        src/org/adligo/models/core/relations/client/rpc/transfer/ResetPasswordRequest.java
        src/org/adligo/models/core/relations/client/rpc/transfer/ResetPasswordResponse.java
        src/org/adligo/models/core/relations/client/rpc/transfer/SaveRequest.java
        src/org/adligo/models/core/relations/client/rpc/transfer/SaveResponse.java
        src/org/adligo/models/core/relations/client/rpc/transfer/SaveUserFailureReasons.java
        src/org/adligo/models/core/relations/client/rpc/transfer/SaveUserGroupFailureReasons.java
        src/org/adligo/models/core/relations/client/rpc/transfer/UserSaveRequest.java
        src/org/adligo/models/core/relations/client/rpc/transfer/UserSaveResponse.java

--- src/org/adligo/models/core/relations/client/rpc/transfer/ResetPasswordFailureReasons.java
+++
@@ -1,5 +0,0 @@
-package org.adligo.models.core.relations.client.rpc.transfer;
-
-public enum ResetPasswordFailureReasons {
-	UNKNOWN_REASON, TOKEN_NOT_VALID
-}
--- src/org/adligo/models/core/relations/client/rpc/transfer/SaveUserFailureReasons.java
+++
@@ -1,6 +0,0 @@
-package org.adligo.models.core.relations.client.rpc.transfer;
-
-public enum SaveUserFailureReasons {
-	UNKNOWN_REASON, REQUEST_NOT_VALID, NEW_USER_ALREADY_EXISTS,
-	NEW_USER_EMAIL_ALREADY_EXISTS_IN_DOMAIN
-}
