095069ea576f9c1b9778f68a2a2eb036faa24c6f
scott <scott@adligo.com>
2010-10-16 18:03:20 +0000

    i am breaking the build extracted code to gwt_model_services project (due to not wanting gwt_util as a dependency here)

--- src/org/adligo/models/core/relations/client/rpc/transfer/EmailAssistanceReponse.java
+++
@@ -1,5 +0,0 @@
-package org.adligo.models.core.relations.client.rpc.transfer;
-
-public enum EmailAssistanceReponse {
-	SENT_SUCCESSFULLY, EMAIL_NOT_FOUND, UNKNOWN_ERROR
-}
--- src/org/adligo/models/core/relations/client/rpc/transfer/SaveUserGroupFailureReasons.java
+++
@@ -1,5 +0,0 @@
-package org.adligo.models.core.relations.client.rpc.transfer;
-
-public enum SaveUserGroupFailureReasons {
-	UNKNOWN_REASON, REQUEST_NOT_VALID, NEW_USER_GROUP_ALREADY_EXISTS
-}
