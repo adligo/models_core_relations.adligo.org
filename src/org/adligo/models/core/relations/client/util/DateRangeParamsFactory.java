e133c30ee4cbcc6e362c6f2534a2a8fd2e65ee8a
scott <scott@adligo.com>
2014-02-04 01:39:06 -0600

    new classes

---
+++ src/org/adligo/models/core/relations/client/util/DateRangeParamsFactory.java
@@ -0,0 +1,106 @@
+package org.adligo.models.core.relations.client.util;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.adligo.i.util.client.DateTime;
+import org.adligo.models.core.client.util.I_DateRange;
+import org.adligo.models.params.client.Params;
+import org.adligo.models.params.client.SqlOperators;
+
+public class DateRangeParamsFactory {
+
+	
+
+	/**
+	 * creates time specific date range params
+	 * @param params
+	 * @param dr
+	 * @param fieldName
+	 */
+	public static void addDateTimeRangeParams(Params params, I_DateRange dr, String fieldName) {
+		Params rangesParams = new Params();
+		params.addParam(fieldName + "_ranges", rangesParams);
+		addDateTimeRange(dr, fieldName, rangesParams);
+	}
+
+	
+	protected static void addDateTimeRange(I_DateRange dr, String fieldName,
+			Params rangesParams) {
+		Params rangeParams = new Params();
+		rangesParams.addParam(fieldName + "_range", rangeParams);
+		Long start = dr.getStart();
+		rangeParams.addParam(fieldName, SqlOperators.GREATER_THAN_EQUALS, new Date(start.longValue()));
+		Long end = dr.getEnd();
+		rangeParams.addParam(fieldName, SqlOperators.LESS_THAN_EQUALS, new Date(end.longValue()));
+	}
+	
+	/**
+	 * creates date range params which include any extra time from the start of the start day 
+	 * to the end of the end day by extending the start to one millisecond before the start day
+	 * and using the GREATER_THAN operator, and extending the end day to the next day and using the 
+	 * LESS_THAN operator.
+	 * @param params
+	 * @param dr
+	 * @param fieldName
+	 */
+	public static void addDateRangeParams(Params params, I_DateRange dr, String fieldName) {
+		Params rangesParams = new Params();
+		params.addParam(fieldName + "_ranges", rangesParams);
+		addDateRange(dr, fieldName, rangesParams);
+	}
+
+	protected static void addDateRange(I_DateRange dr, String fieldName,
+			Params rangesParams) {
+		Params rangeParams = new Params();
+		rangesParams.addParam(fieldName + "_range", rangeParams);
+		DateTime startDt = new DateTime(dr.getStart());
+		DateTime startP = new DateTime(startDt.getDayStart());
+		startP = new DateTime(startP.getTime() - 1L);
+		
+		//System.out.println(new DateTime(start));
+		DateTime endDt = new DateTime(dr.getEnd());
+		DateTime endP = new DateTime(endDt.getDayEnd());
+		endP = new DateTime(endP.getTime() + 1L);
+		
+		Date startDate = new Date(startP.getTime());
+		Date endDate = new Date(endP.getTime());
+		rangeParams.addParam(fieldName, SqlOperators.GREATER_THAN, startDate);
+		rangeParams.addParam(fieldName, SqlOperators.LESS_THAN, endDate);
+	}
+	
+	/**
+	 * Note no generics or annotations for jme compaitibility
+	 * 
+	 * @param params
+	 * @param drs
+	 * @param fieldName
+	 */
+	public static void addDateTimeRangeParams(Params params, Collection<I_DateRange> drs, String fieldName) {
+		if (drs.size() > 0) {
+			Params rangesParams = new Params();
+			params.addParam(fieldName + "_ranges", rangesParams);
+			for (I_DateRange dr: drs) {
+				DateRangeParamsFactory.addDateTimeRange(dr, fieldName, rangesParams);
+			}
+		}
+	}
+	
+	/**
+	 * note no Generics or annotations for jme
+	 * 
+	 * @param params
+	 * @param drs
+	 * @param fieldName
+	 */
+	public static void addDateRangeParams(Params params, Collection<I_DateRange> drs, String fieldName) {
+		if (drs.size() > 0) {
+			Params rangesParams = new Params();
+			params.addParam(fieldName + "_ranges", rangesParams);
+			
+			for (I_DateRange dr: drs) {
+				addDateRange(dr, fieldName, rangesParams);
+			}
+		}
+	}
+}
---
+++ src/org/adligo/models/core/relations/client/util/SIDParamFactory.java
@@ -0,0 +1,250 @@
+package org.adligo.models.core.relations.client.util;
+
+import java.util.Collection;
+
+import org.adligo.models.core.client.ids.I_LongIdentifier;
+import org.adligo.models.core.client.ids.I_StorageIdentifier;
+import org.adligo.models.core.client.ids.I_StringIdentifier;
+import org.adligo.models.core.client.ids.I_VersionedLongIdentifier;
+import org.adligo.models.params.client.I_TemplateParams;
+import org.adligo.models.params.client.Param;
+import org.adligo.models.params.client.Params;
+import org.adligo.models.params.client.ParamsFactory;
+import org.adligo.models.params.client.SqlOperators;
+
+public class SIDParamFactory {
+
+	public static final String VERSION = "version";
+
+	public static I_TemplateParams byId(I_StorageIdentifier p) {
+		try {
+			return byId((I_LongIdentifier) p);
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		try {
+			return byId((I_VersionedLongIdentifier) p);
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		try {
+			return byId((I_StringIdentifier) p);
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		return null;
+	}
+	
+	private static I_TemplateParams byId(I_LongIdentifier p) {
+		Long id = p.getId();
+		return ParamsFactory.byId(id.longValue());
+	}
+	
+	private static I_TemplateParams byId(I_StringIdentifier p) {
+		String id = p.getKey();
+		return ParamsFactory.byId(id);
+	}
+	
+	private static I_TemplateParams byId(I_VersionedLongIdentifier p) {
+		Params params = new Params();
+		params.addParam(ParamsFactory.DEFAULT);
+		Params whereParams = new Params();
+		params.addParam(ParamsFactory.WHERE, whereParams);
+		
+		Long id = p.getId();
+		if (id == null) {
+			whereParams.addParam(ParamsFactory.ID, SqlOperators.IS_NULL);
+		} else {
+			whereParams.addParam(ParamsFactory.ID, SqlOperators.EQUALS, id.longValue());
+		}
+		
+		Integer version = p.getVersion();
+		if (version == null) {
+			whereParams.addParam(VERSION, SqlOperators.IS_NULL);
+		} else {
+			whereParams.addParam(VERSION, SqlOperators.EQUALS, version.intValue());
+		}
+		return params;
+	}
+	
+
+	public static void addIdParameter(Params parent, String paramName, I_StorageIdentifier p) {
+		try {
+			addIdParameter(parent, paramName, (I_LongIdentifier) p);
+			return;
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		try {
+			addIdParameter(parent, paramName, (I_StringIdentifier) p);
+			return;
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		try {
+			addIdParameter(parent, paramName, (I_VersionedLongIdentifier) p);
+			return;
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+	}
+	
+	public static void addIdParameter(Params parent, String paramName, I_StorageIdentifier p, I_TemplateParams child) {
+		try {
+			addIdParameter(parent, paramName, (I_LongIdentifier) p, child);
+			return;
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		try {
+			addIdParameter(parent, paramName, (I_StringIdentifier) p, child);
+			return;
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		try {
+			addIdParameter(parent, paramName, (I_VersionedLongIdentifier) p, child);
+			return;
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+	}
+	/**
+	 * 
+	 * @param parent the parent parameters (whereParams usually)
+	 * @param paramName 
+	 * @param p
+	 */
+	public static void addIdParameter(Params parent, String paramName, I_LongIdentifier p) {
+		Long id = p.getId();
+		if (id == null) {
+			parent.addParam(paramName, SqlOperators.IS_NULL);
+		} else {
+			parent.addParam(paramName, SqlOperators.EQUALS, id.longValue());
+		}
+	}
+	
+	public static void addIdParameter(Params parent, String paramName, I_LongIdentifier p, I_TemplateParams childParams) {
+		Long id = p.getId();
+		if (id == null) {
+			Param param = parent.addParam(paramName, SqlOperators.IS_NULL);
+			param.setParams(childParams);
+		} else {
+			Param param = parent.addParam(paramName, SqlOperators.EQUALS, id.longValue());
+			param.setParams(childParams);
+		}
+	}
+	/**
+	 * 
+	 * @param parent the parent parameters (whereParams usually)
+	 * @param paramName 
+	 * @param p
+	 */
+	public static void addIdParameter(Params parent, String paramName, I_StringIdentifier p) {
+		String id = p.getKey();
+		if (id == null) {
+			parent.addParam(paramName, SqlOperators.IS_NULL);
+		} else {
+			parent.addParam(paramName, SqlOperators.EQUALS, id);
+		}
+	}
+	
+	public static void addIdParameter(Params parent, String paramName, I_StringIdentifier p, I_TemplateParams child) {
+		String id = p.getKey();
+		if (id == null) {
+			Param param = parent.addParam(paramName, SqlOperators.IS_NULL);
+			param.setParams(child);
+		} else {
+			Param param = parent.addParam(paramName, SqlOperators.EQUALS, id);
+			param.setParams(child);
+		}
+		
+	}
+	
+	public static void addIdParameter(Params parent, String idParamName, String valueParamName,  I_VersionedLongIdentifier p) {
+		Long id = p.getId();
+		if (id == null) {
+			parent.addParam(idParamName, SqlOperators.IS_NULL);
+		} else {
+			parent.addParam(idParamName, SqlOperators.EQUALS, id.longValue());
+		}
+		
+		Integer version = p.getVersion();
+		if (version == null) {
+			parent.addParam(valueParamName, SqlOperators.IS_NULL);
+		} else {
+			parent.addParam(valueParamName, SqlOperators.EQUALS, version.intValue());
+		}
+	}
+	
+	public static void addIdParameter(Params parent, String idParamName, String valueParamName,  I_VersionedLongIdentifier p, I_TemplateParams child) {
+		Long id = p.getId();
+		if (id == null) {
+			Param param = parent.addParam(idParamName, SqlOperators.IS_NULL);
+			param.setParams(child);
+		} else {
+			Param param = parent.addParam(idParamName, SqlOperators.EQUALS, id.longValue());
+			param.setParams(child);
+		}
+		
+		Integer version = p.getVersion();
+		if (version == null) {
+			Param param = parent.addParam(valueParamName, SqlOperators.IS_NULL);
+			param.setParams(child);
+		} else {
+			Param param = parent.addParam(valueParamName, SqlOperators.EQUALS, version.intValue());
+			param.setParams(child);
+		}
+	}
+	
+	public static Object getIdParameter(I_LongIdentifier p) {
+		return p.getId();
+	}
+	
+	/**
+	 * 
+	 * @param parent the parent parameters (whereParams usually)
+	 * @param paramName 
+	 * @param p
+	 */
+	public static Object getIdParameter(I_StringIdentifier p) {
+		return p.getKey();
+	}
+	
+	public static Object getIdParameter(I_StorageIdentifier p) {
+		try {
+			return getIdParameter((I_LongIdentifier) p);
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		try {
+			return getIdParameter((I_StringIdentifier) p);
+		} catch (ClassCastException cce) {
+			//do nothing
+		}
+		throw new IllegalArgumentException("Unable to obtain a single object id for " + p);
+	}
+
+	/**
+	 * Note no generics or annotations for jme compatibility
+	 * @param parent
+	 * @param paramName
+	 * @param p a colletion of I_StorageIdentifier
+	 */
+	public static void addIdParametersInClauses(Params parent, String paramName, Collection<I_StorageIdentifier> p) {
+		Param childParam = parent.addParam(paramName,SqlOperators.IN);
+		
+	    for (I_StorageIdentifier id: p) {
+			Object obj = SIDParamFactory.getIdParameter(id);
+			try {
+				childParam.addValue((Long) obj);
+			} catch (ClassCastException x) {
+				try {
+					childParam.addValue((String) obj);
+				} catch (ClassCastException g) {
+					throw new RuntimeException(g);
+				}
+			}
+		}
+	}
+}
